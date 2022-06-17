218d8aa5452dbb2f71296b7403ddf615bb27475c
ravenkim <ravenkim97@naver.com>
2022-06-18 00:39:58 +0900

    유

--- src/db.js
+++ src/db.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
 
 const db = mongoose.connection;
