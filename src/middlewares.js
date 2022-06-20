13c4cf3458c06cfee82a8c8f820531f48237b080
ravenkim <ravenkim97@naver.com>
2022-06-20 21:31:57 +0900

    ㅋㅌㅊ

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,12 +1,33 @@
 import multer from "multer";
+import multerS3  from "multer-s3";
+import aws from "aws-sdk";
 
-//const isHeroku = process.env.NODE_ENV === "production";
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ImageUploader = multerS3({
+  s3: s3,
+  bucket: "wetubeee/images",
+  acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "wetubeee/videos",
+  acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "WIHI HomePage";
   res.locals.loggedInUser = req.session.user || {};
-  //res.locals.isHeroku = isHeroku;
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -34,7 +55,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 4000000,
   },
-  //storage: isHeroku ? s3ImageUploader : undefined,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
@@ -42,5 +63,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 20000000,
   },
-  //storage: isHeroku ? s3VideoUploader : undefined,
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
