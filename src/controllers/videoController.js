0f5b7390d5db5740c4914f5961e09effeac5e6dd
ravenkim <ravenkim97@naver.com>
2022-07-11 00:13:24 +0900

    업로드

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,14 +26,14 @@ export const postUpload = async (req, res) => {
     user: { _id },
   } = req.session;
   const { video, thumb } = req.files;
-  
   const { title, description, hashtags } = req.body;
+  const isHeroku = process.env.NODE_ENV === "production";
   try {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl:  video[0].path.replace(/[\\]/g, "/"),
-      thumbUrl: thumb[0].path.replace(/[\\]/g, "/"),
+      fileUrl: isHeroku ? video[0].location : video[0].path,
+      thumbUrl: isHeroku ? thumb[0].location : video[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
