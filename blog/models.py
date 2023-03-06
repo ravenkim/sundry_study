64297b607dc0d28fceb40c840466efc9ed43c216
ravenkim <41793830+ravenkim@users.noreply.github.com>
2023-03-06 14:16:16 +0900

    자세히 보기 완효

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,6 @@ class Post(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    text = models.TextField
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
