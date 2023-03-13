207f50323df3c64945a1d158c6f88d7ac30f39e4
ravenkim <41793830+ravenkim@users.noreply.github.com>
2023-03-13 09:11:05 +0900

    ddd

--- askcompany/accounts/urls.py
+++ askcompany/accounts/urls.py
@@ -1,7 +1,9 @@
-
+from django.contrib.auth.views import LoginView
+from django.urls import path
 
 app_name = 'accounts'
 
+
 urlpatterns = [
-    
+    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
 ]
