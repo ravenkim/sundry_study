69856b9febfb9dfa51ce0d79edf1eef947d2ae1e
ravenkim <41793830+ravenkim@users.noreply.github.com>
2023-03-13 13:45:35 +0900

    회원관리

--- askcompany/askcompany/settings.py
+++ askcompany/askcompany/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -158,3 +160,5 @@ MESSAGE_TAGS = {
 }
 
 AUTH_USER_MODEL = 'auth.User'
+
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
