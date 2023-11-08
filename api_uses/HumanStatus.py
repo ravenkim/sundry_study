3df8093334815842bf336fb1641d73515f3bbedd
sang981113 <75288953+sang981113@users.noreply.github.com>
2023-11-08 22:16:58 +0900

    api use examples

---
+++ api_uses/HumanStatus.py
@@ -0,0 +1,31 @@
+#-*- coding:utf-8 -*-
+import urllib3
+import json
+import base64
+openApiURL = "http://aiopen.etri.re.kr:8000/HumanStatus"
+accessKey = "8972d09b-9538-44a2-bf3d-f51c50e2edf0"
+imageFilePath = "api_uses\sample\humanstatus01.jpg"
+type = "jpg"
+
+file = open(imageFilePath, "rb")
+imageContents = base64.b64encode(file.read()).decode("utf8")
+file.close()
+
+requestJson = {
+"argument": {
+    "type": type,
+    "file": imageContents
+}
+}
+
+http = urllib3.PoolManager()
+response = http.request(
+"POST",
+openApiURL,
+headers={"Content-Type": "application/json; charset=UTF-8","Authorization": accessKey},
+body=json.dumps(requestJson)
+)
+
+print("[responseCode] " + str(response.status))
+print("[responBody]")
+print(response.data)
---
+++ api_uses/VideoParse.py
@@ -0,0 +1,31 @@
+#-*- coding:utf-8 -*-
+import urllib3
+import json
+import os
+
+openApiURL = "http://aiopen.etri.re.kr:8000/VideoParse"
+accessKey = "8972d09b-9538-44a2-bf3d-f51c50e2edf0"
+videoFilePath = "api_uses\sample\demo-1.mp4"
+
+file = open(videoFilePath,'rb')
+fileContent = file.read()
+file.close()
+
+requestJson = {	
+    "argument": {}
+}
+
+http = urllib3.PoolManager()
+response = http.request(
+    "POST",
+    openApiURL,
+    headers={"Content-Type": "application/json; charset=UTF-8", "Authorization" :  accessKey},
+    fields={
+        'json': json.dumps(requestJson),
+        'uploadfile': (os.path.basename(file.name), fileContent)
+    }
+)
+
+print("[responseCode] " + str(response.status))
+print("[responBody]")
+print(response.data)
