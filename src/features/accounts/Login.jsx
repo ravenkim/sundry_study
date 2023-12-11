5cd6dd0a0bb37d8758a96fbcd039474bab5d197f
cmpark <cmpark@euclidsoft.co.kr>
2023-12-11 17:43:06 +0900

    [추가] 임시 로그인 기능 이메일 유효성 추가

--- src/features/accounts/Login.jsx
+++ src/features/accounts/Login.jsx
@@ -15,26 +15,37 @@ const Login = () => {
     const [userPassword, setUserPassword] = useState("")
     const dispatch = useDispatch()
 
+    const validateEmail = (email) => {
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return emailPattern.test(String(email).toLowerCase());
+    }; // 이메일 형식 유효성 검사
 
     const loginHandler = async () => {
         console.log('실행중')
-    try {
-      const response = await axios.post('url', {
-        userID,
-        userPassword,
-      });
-      const token = response.data.token; // 서버로부터 받은 JWT 토큰
 
-      // 받은 토큰을 로컬 스토리지에 저장
-      localStorage.setItem('token', token);
+        if (!validateEmail(userID)) {
+            /*message.error('올바른 이메일 형식이 아닙니다.');*/
+            alert("올바른 이메일 형식이 아닙니다.")
+            return;
+        } // 이메일 형식이 아닐 경우
 
-      // 로그인 성공 후 작업 실행
+        try {
+            const response = await axios.post('url', {
+                userID,
+                userPassword,
+            });
+            const token = response.data.token; // 서버로부터 받은 JWT 토큰
 
-    } catch (error) {
-      // 로그인 실패 처리
-      console.error('로그인 실패:', error);
-    }
-  };
+            // 받은 토큰을 로컬 스토리지에 저장
+            localStorage.setItem('token', token);
+
+            // 로그인 성공 후 작업 실행
+
+        } catch (error) {
+            // 로그인 실패 처리
+            console.error('로그인 실패:', error);
+        }
+    };
 
 
     useEffect(() => {
@@ -78,34 +89,34 @@ const Login = () => {
                               loginHandler()
                           }}
                     >
-                    <Input
-                        value={userID}
-                        onChange={(e) => setUserID(e.target.value)}
-                        prefix={<UserOutlined/>}
-                        placeholder="아이디를 입력하세요."
-                        style={{
-                            marginBottom: '25px',
-                            height: '40px',
-                        }}
-                    />
+                        <Input
+                            value={userID}
+                            onChange={(e) => setUserID(e.target.value)}
+                            prefix={<UserOutlined/>}
+                            placeholder="아이디를 입력하세요."
+                            style={{
+                                marginBottom: '25px',
+                                height: '40px',
+                            }}
+                        />
 
-                    <Input.Password
-                        prefix={<LockOutlined/>}
-                        style={{
-                            marginBottom: '50px',
-                            height: '40px',
-                        }}
-                        onChange={(e)=> setUserPassword(e.target.value)}
-                        placeholder="비밀번호를 입력하세요."/>
-                    <SSbutton
-                        type='primary'
-                        htmlType={"submit"}
-                        block
-                        className={`h-[60px] text-[20px] font-bold bg-[#4F5FF5] mb-[25px] text-[#ffffff] hover:bg-[#263BF2]`}
-                    >
-                        로그인
-                    </SSbutton>
-                        </form>
+                        <Input.Password
+                            prefix={<LockOutlined/>}
+                            style={{
+                                marginBottom: '50px',
+                                height: '40px',
+                            }}
+                            onChange={(e) => setUserPassword(e.target.value)}
+                            placeholder="비밀번호를 입력하세요."/>
+                        <SSbutton
+                            type='primary'
+                            htmlType={"submit"}
+                            block
+                            className={`h-[60px] text-[20px] font-bold bg-[#4F5FF5] mb-[25px] text-[#ffffff] hover:bg-[#263BF2]`}
+                        >
+                            로그인
+                        </SSbutton>
+                    </form>
                     <div className='flex w-full justify-center items-center'>
                         <ul className='flex flex-row gap-[10px] tablet:gap-[16px] text-[#51525C] items-center justify-center'>
                             <li onClick={() => dispatch(push("/searchId"))} className='cursor-pointer'>아이디 찾기</li>
