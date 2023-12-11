de90f9d2a880585aaf60910ac56652db9e7ed9ae
cmpark <cmpark@euclidsoft.co.kr>
2023-12-11 17:34:21 +0900

    [기능] 임시 로그인 기능 작성

--- src/features/accounts/Login.jsx
+++ src/features/accounts/Login.jsx
@@ -5,6 +5,7 @@ import {UserOutlined, LockOutlined} from '@ant-design/icons';
 import {useEffect, useState} from "react";
 import {useDispatch} from "react-redux";
 import MainLogoSvg from "../../common/components/Svg/MainLogoSvg.jsx";
+import axios from "axios";
 
 
 const Login = () => {
@@ -15,9 +16,25 @@ const Login = () => {
     const dispatch = useDispatch()
 
 
-    const loginHandler = () => {
+    const loginHandler = async () => {
+        console.log('실행중')
+    try {
+      const response = await axios.post('url', {
+        userID,
+        userPassword,
+      });
+      const token = response.data.token; // 서버로부터 받은 JWT 토큰
 
+      // 받은 토큰을 로컬 스토리지에 저장
+      localStorage.setItem('token', token);
+
+      // 로그인 성공 후 작업 실행
+
+    } catch (error) {
+      // 로그인 실패 처리
+      console.error('로그인 실패:', error);
     }
+  };
 
 
     useEffect(() => {
@@ -55,9 +72,15 @@ const Login = () => {
                         <h2 className='text-[#111321] text-[22px] tablet:text-[26px] desktop:text-[30px]'>안녕하세요 :)</h2>
                         <h2 className='text-[#111321] text-[22px] tablet:text-[26px] desktop:text-[30px]'>RMS 입니다.</h2>
                     </div>
+                    <form action=""
+                          onSubmit={(e) => {
+                              e.preventDefault()
+                              loginHandler()
+                          }}
+                    >
                     <Input
                         value={userID}
-                        onChange={(e) => setUserID(e.value)}
+                        onChange={(e) => setUserID(e.target.value)}
                         prefix={<UserOutlined/>}
                         placeholder="아이디를 입력하세요."
                         style={{
@@ -72,14 +95,17 @@ const Login = () => {
                             marginBottom: '50px',
                             height: '40px',
                         }}
+                        onChange={(e)=> setUserPassword(e.target.value)}
                         placeholder="비밀번호를 입력하세요."/>
                     <SSbutton
                         type='primary'
+                        htmlType={"submit"}
                         block
-                        className={`h-[60px] text-[20px] font-bold bg-[#4F5FF5] mb-[25px]`}
+                        className={`h-[60px] text-[20px] font-bold bg-[#4F5FF5] mb-[25px] text-[#ffffff] hover:bg-[#263BF2]`}
                     >
                         로그인
                     </SSbutton>
+                        </form>
                     <div className='flex w-full justify-center items-center'>
                         <ul className='flex flex-row gap-[10px] tablet:gap-[16px] text-[#51525C] items-center justify-center'>
                             <li onClick={() => dispatch(push("/searchId"))} className='cursor-pointer'>아이디 찾기</li>
