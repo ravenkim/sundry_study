6f0cdb20aa8e7b4f42624e277be4220541b634a2
unknown <41793830+ravenkim@users.noreply.github.com>
2023-12-11 17:28:28 +0900

    [추가] 공통 검색 생성

--- src/common/components/input/SSsearchInput.jsx
+++ src/common/components/input/SSsearchInput.jsx
@@ -1,53 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Input} from "antd";
 
 const SSsearchInput = ({
-    children,
-    inputStyle,
-    containerStyle,
-    title,
+
+  title,
+  onPressEnter,
     onChange,
-    onPressEnter,
-    placeholder = "",
-    inputOtherProps
+    value,
+  placeholder = "",
+      inputStyle,
+  containerStyle,
+  inputOtherProps
 }) => {
-    return (
-        <div
-            style={{
-                marginTop: '40px',
-                width: '800px',
-                height: '118px',
-                display: "flex",
-                flexDirection: "column",
-                ...containerStyle
-            }}
-        >
-            <h1>
-                {title}
-            </h1>
 
+  return (
+    <div
+      style={{
+        marginTop: '40px',
+        width: '800px',
+        height: '118px',
+        display: "flex",
+        flexDirection: "column",
+        ...containerStyle
+      }}
+    >
+      <h1>
+        {title}
+      </h1>
 
-            <Input
-                style={{
-                    marginBottom: '20px',
-                    ...inputStyle,
-                }}
-                placeHolder={{}}
-                onChange={onChange}
-                placeholder={placeholder}
-                onPressEnter={onPressEnter}
-                {...inputOtherProps}
-            >
-
-
-                {children}
-            </Input>
-
-        </div>
-
-
-    );
+      <Input
+        style={{
+          marginBottom: '20px',
+          ...inputStyle,
+        }}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onPressEnter={onPressEnter}
+        {...inputOtherProps}
+      >
+      </Input>
+    </div>
+  );
 };
-
 
 export default SSsearchInput;
--- src/features/door/Door.jsx
+++ src/features/door/Door.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SSsearchInput from "../../common/components/input/SSsearchInput.jsx";
+import {Input} from "antd";
 
 const Door = () => {
+
+    const [serchAllText, setSerchAllText] = useState()
+
     return (
         <>
             <SSsearchInput
+                value ={serchAllText}
+                onChange={(e) => setSerchAllText(e.target.value)}
+                     placeholder={'찾고자 하는 항목을 입력하세요. (전체 항목에서 검색됩니다)'}
                 title={'무엇을 찾으시나요?'}
-                placeholder={'전체 항목에서 검색합니다'}
             />
+
+
+
+
 
             <div
                 style={{
                     width: '800px'
                 }}
             >
-                원하는 서비스로 가장 빠르게 이동해 보세요.
+                <h2>원하는 서비스로 가장 빠르게 이동해 보세요.</h2>
 
             </div>
         </>
