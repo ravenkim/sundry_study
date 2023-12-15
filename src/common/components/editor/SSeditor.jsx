2566cef23b537eb8fbfb68ebda6e1be95d4342f9
unknown <41793830+ravenkim@users.noreply.github.com>
2023-12-15 11:27:32 +0900

    Revert "[에디터ㅏ 추가"
    This reverts commit 932bf35f49dd1172e47094465395d50742daca5e.

--- src/common/components/editor/SSeditor.jsx
+++ src/common/components/editor/SSeditor.jsx
@@ -1,29 +1,10 @@
 import React from 'react';
-import {LIME_EDITOR_OPTIONS} from "./EditorConstants.js";
-import SunEditor from 'suneditor-react';
 
-
-
-const SSeditor = ({height = "500px", autoFocus, isEditMode, initContents, changeHandler, customOptions}) => {
+const SSeditor = () => {
     return (
-        <SunEditor
-            lang={ko}
-            autoFocus={autoFocus}
-            disable={!isEditMode}
-            hideToolbar={!isEditMode}
-            setContents={initContents}
-            height={height}
-            setDefaultStyle={`font-family: 'Malgun Gothic'; font-size: 14px;`}
-            onChange={(contents) => {
-                if(changeHandler && typeof changeHandler === "function"){
-                    changeHandler(contents);
-                }
-            }}
-            setOptions={{
-                ...LIME_EDITOR_OPTIONS,
-                ...customOptions,
-            }}
-          />
+        <div>
+            
+        </div>
     );
 };
 
