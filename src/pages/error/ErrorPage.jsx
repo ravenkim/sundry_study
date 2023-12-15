7988b14095a8af2d53e3d28f4b6d9fd19fa2a4fb
unknown <41793830+ravenkim@users.noreply.github.com>
2023-12-15 09:24:01 +0900

    [추가] 404 페이지

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import SSlayout from "../../common/components/layout/SSlayout.jsx";
+import SSlayout from "src/common/components/layout/SSlayout.jsx";
+import SSbutton from "src/common/components/button/SSbutton.jsx";
 
 const ErrorPage = () => {
     return (
         <SSlayout>
-            오류페이지
+            <h1>
+                404 
+            </h1>
+            
+            <h2>잘못된 url로 들어오셨군요!</h2>
+            <h2>존재 하지 않는 페이지 입니다.</h2>
+            
+            <SSbutton>
+                홈으로 돌아가기
+            </SSbutton>
+            
 
         </SSlayout>
     );
