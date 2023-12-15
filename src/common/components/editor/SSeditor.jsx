932bf35f49dd1172e47094465395d50742daca5e
unknown <41793830+ravenkim@users.noreply.github.com>
2023-12-15 11:24:34 +0900

    [에디터ㅏ 추가

--- src/common/components/editor/SSeditor.jsx
+++ src/common/components/editor/SSeditor.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import {LIME_EDITOR_OPTIONS} from "./EditorConstants.js";
+import SunEditor from 'suneditor-react';
 
-const SSeditor = () => {
+
+
+const SSeditor = ({height = "500px", autoFocus, isEditMode, initContents, changeHandler, customOptions}) => {
     return (
-        <div>
-            
-        </div>
+        <SunEditor
+            lang={ko}
+            autoFocus={autoFocus}
+            disable={!isEditMode}
+            hideToolbar={!isEditMode}
+            setContents={initContents}
+            height={height}
+            setDefaultStyle={`font-family: 'Malgun Gothic'; font-size: 14px;`}
+            onChange={(contents) => {
+                if(changeHandler && typeof changeHandler === "function"){
+                    changeHandler(contents);
+                }
+            }}
+            setOptions={{
+                ...LIME_EDITOR_OPTIONS,
+                ...customOptions,
+            }}
+          />
     );
 };
 
