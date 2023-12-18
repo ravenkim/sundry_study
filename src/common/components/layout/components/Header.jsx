ef047fd8fd3d3b3adba43d6a52724c13afd1250e
cmpark <cmpark@euclidsoft.co.kr>
2023-12-18 15:35:24 +0900

    [추가] 헤더 프로필 버튼 생성

--- src/common/components/layout/components/Header.jsx
+++ src/common/components/layout/components/Header.jsx
@@ -2,6 +2,8 @@ import {CloseOutlined, MenuOutlined} from "@ant-design/icons";
 import {shallowEqual, useSelector} from "react-redux";
 import {useDispatch} from "react-redux";
 import {push} from "redux-first-history";
+import React, {useEffect, useState} from "react";
+import SSbutton from "../../button/SSbutton.jsx";
 
 const Header = ({
                     setMenuOpen,
@@ -19,6 +21,12 @@ const Header = ({
         shallowEqual
     );
 
+    const [imageSrc, setImageSrc] = useState('');
+    const [profileOpen, setProfileOpen] = useState(false)
+
+    useEffect(() => {
+        setImageSrc('/src/assets/img/profile_example.png')
+    }, []);
 
     return (
         <header
@@ -76,17 +84,69 @@ const Header = ({
 
 
             </div>
+
             {/*프로필 영역*/}
-            <div
-                style={{
-                    width: '63px',
-                    height: '63px',
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center"
-                }}>
+            <div className={'w-[63px] h-[63px] flex justify-center items-center relative'}>
+                <div className={'after:overflow-hidden rounded-full overflow-hidden max-w-[63px] max-h-[63px] flex justify-center items-center cursor-pointer bg-[#ffffff]'}
+                     onClick={()=> setProfileOpen(!profileOpen)}
+                >
+                    <img src={imageSrc} alt="#" className={'max-w-[40px] max-h-[auto]'}/>
+                </div>
 
             </div>
+            {/*오픈영역*/}
+                {profileOpen &&
+                    <div className={'absolute top-[70px] right-[20px] tablet:right-[23px] text-[#000000] rounded-[10px] overflow-hidden shadow-[0px_1px_4px_rgba(0,0,0,0.3)] box-border w-[calc(100%-40px)] tablet:w-fit tablet:min-w-[400px] desktop:min-w-[300px] z-[100] bg-[#ffffff]'}>
+                        <div className={'relative w-full flex flex-col justify-center items-center bg-[#5565F6] bg-opacity-60 p-[20px]'}>
+                            <h4 className={'text-[#ffffff] pb-[50px]'}>일반 사용자</h4>
+                            <div className={'absolute rounded-full overflow-hidden border-[#ffffff] border-2 box-border border-solid -bottom-1/2 -translate-y-1/2 cursor-pointer'}>
+                                <img src={imageSrc} alt="#" className={'max-w-[63px] h-auto'}
+                                     onClick={()=>{
+                                         console.log('프로필 수정 화면으로 이동')
+                                     }}
+                                />
+                            </div>
+                        </div>
+                        <div className={'p-[20px] pt-[50px] flex flex-col justify-center items-center gap-[8px]' }>
+                            <div className={'flex justify-center items-center flex-col'}>
+                                <h5>박찬민</h5>
+                                <h5>cksals123@gmail.com</h5>
+                            </div>
+                            <SSbutton onClick={()=> {
+                                /*내 정보 페이지로 이동*/
+                            }}
+                                      className={'w-full'}
+                            >
+                                내 정보 수정
+                            </SSbutton>
+
+                            <SSbutton  onClick={()=> {
+                                /*관심목록으로 이동*/
+                            }}
+                                       className={'w-full'}
+                            >
+                                관심 목록
+                            </SSbutton>
+
+                            <SSbutton  onClick={()=> {
+                                /*대여 목록으로 이동*/
+                            }}
+                                       className={'w-full'}
+                            >
+                                대여 목록
+                            </SSbutton>
+
+                            {/*<SSbutton  onClick={()=> {
+                            }}>
+                                관리자 페이지로 이동
+                            </SSbutton>*/}
+
+                            <SSbutton danger className={'w-full'} onClick={()=>{
+                                console.log('로그아웃 후 메인 화면으로 이동')
+                            }}>로그아웃</SSbutton>
+                        </div>
+                    </div>
+                }
 
 
         </header>
