a91fff5d29231cb9dbeb1322f52be5a995ed7295
unknown <41793830+ravenkim@users.noreply.github.com>
2023-12-19 09:43:18 +0900

    로그아웃 추가

--- src/common/utils/redux/loginUtils.jsx
+++ src/common/utils/redux/loginUtils.jsx
@@ -1,8 +1,7 @@
 import store from "/src/app/store.jsx";
-import { jwtDecode } from "jwt-decode";
+import {jwtDecode} from "jwt-decode";
 import {userAction} from "src/features/accounts/userReducer.jsx";
 import {getCookie} from "src/app/cookie.jsx";
-
 
 
 const jwtDecodeHandler = (data) => {
@@ -11,12 +10,23 @@ const jwtDecodeHandler = (data) => {
 
 
 export const setUserHandler = () => {
-  try {
-    const userToken = getCookie('tk')  // 쿠키에서 가져오지
-    if (!userToken) return;
-    store.dispatch(userAction.setUser(jwtDecodeHandler(userToken)))
+    try {
+        const userToken = getCookie('tk')  // 쿠키에서 가져오지
+        if (!userToken) return;
+        store.dispatch(userAction.setUser(jwtDecodeHandler(userToken)))
+    } catch (e) {
+        console.error("User Setting Exception : ", e);
+    }
+};
 
-  } catch (e) {
-    console.error("User Setting Exception : ", e);
-  }
+
+export const logoutHandler = () => {
+    try {
+        let user = getCookie('tk')
+        if (user) {
+            //  리프레시 토큰 삭제
+        }
+    } catch (e) {
+        console.error("Logout Exception : ", e);
+    }
 };
