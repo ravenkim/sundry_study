1b7e360b77f6e0cc5abd3eff28f86a1e02979dc8
cmpark <cmpark@euclidsoft.co.kr>
2023-12-23 13:21:28 +0900

    [임시] 렌탈 페이지 작성중 // 소현님과 데이터 확인 후 추가 작업 진행

--- src/features/profile/components/RentalsInfo.jsx
+++ src/features/profile/components/RentalsInfo.jsx
@@ -1,10 +1,16 @@
 import React, {useEffect} from 'react';
 import {shallowEqual, useDispatch, useSelector} from "react-redux";
+import SSsearchInput from "../../../common/components/input/SSsearchInput.jsx";
 
 const RentalsInfo = () => {
+    const dispatch = useDispatch()
+
     return(
         <>
-            RentalsInfo
+            <div className={'w-full flex '}>
+
+                <SSsearchInput inputClassName={'h-[40px]'} iconClassName={'h-[20px]'} svgWidth={'16px'} />
+            </div>
         </>
     )
 }
