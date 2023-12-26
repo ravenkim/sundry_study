bcf398b8087f795ccb1a676123e064d21850e622
cmpark <cmpark@euclidsoft.co.kr>
2023-12-26 14:04:27 +0900

    [작성] userInfo 이미지 업로드 / 패스워드 변경 로직 작성 완료 및 코드 정리

--- src/features/profile/components/UserInfo.jsx
+++ src/features/profile/components/UserInfo.jsx
@@ -43,42 +43,36 @@ const UserInfo = () => {
     }, []);
 
     const validatePassword = (password) => {
-    const MIN = 8;
-    const MAX = 20;
+        const MIN = 8; // 최소값
+        const MAX = 20; // 최대값
 
-    const hasDigit = /[0-9]/.test(password);
-    const hasLetter = /[a-zA-Z]/.test(password);
-    const hasSpecialChar = /[\W_]/.test(password);
-    const isLengthValid = password.length >= MIN && password.length <= MAX;
-    /*const pwPattern = /^(?=.*\\d)(?=.*[a-zA-Z])(?=.*[\\W])(?!.*(.)\\1\\1)(?!.*\\d\\d\\d)(?!.*[a-zA-Z]\\1\\1)[\\da-zA-Z\\W]{8,20}$/;
-    return pwPattern.test(password);*/
+        const hasDigit = /[0-9]/.test(password);
+        const hasLetter = /[a-zA-Z]/.test(password);
+        const hasSpecialChar = /[\W_]/.test(password);
+        const isLengthValid = password.length >= MIN && password.length <= MAX;
+        /*const pwPattern = /^(?=.*\\d)(?=.*[a-zA-Z])(?=.*[\\W])(?!.*(.)\\1\\1)(?!.*\\d\\d\\d)(?!.*[a-zA-Z]\\1\\1)[\\da-zA-Z\\W]{8,20}$/;
+        return pwPattern.test(password);*/
 
-    if (!hasDigit) {
-        return '비밀번호에 숫자가 포함되어야 합니다.';
-    }
-    if (!hasLetter) {
-        return '비밀번호에 영문자가 포함되어야 합니다.';
-    }
-    if (!hasSpecialChar) {
-        return '비밀번호에 특수문자가 포함되어야 합니다.';
-    }
-    if (!isLengthValid) {
-        return '비밀번호는 8자에서 20자 사이여야 합니다.';
-    }
-    /*if (!pwPattern) {
-        return '유효한 비밀번호 형식이 아닙니다.';
-    }*/
+        if (!hasDigit) {
+            return '비밀번호에 숫자가 포함되어야 합니다.';
+        }
+        if (!hasLetter) {
+            return '비밀번호에 영문자가 포함되어야 합니다.';
+        }
+        if (!hasSpecialChar) {
+            return '비밀번호에 특수문자가 포함되어야 합니다.';
+        }
+        if (!isLengthValid) {
+            return '비밀번호는 8자에서 20자 사이여야 합니다.';
+        }
+        /*if (!pwPattern) {
+            return '유효한 비밀번호 형식이 아닙니다.';
+        }*/
 
-    return null; // 유효한 경우
-};
+        return null; // 유효한 경우
+    };
 
     const onSave = () => {
-        /*if (selectedImage) {
-
-            dispatch(profileAction.postUserProfileImg(selectedImage)); // 이미지 업로드 액션
-        } else {
-            console.log('파일이 없습니다.');
-        }*/
 
         if (fileList.length > 0) {
             const formData = new FormData();
@@ -100,7 +94,6 @@ const UserInfo = () => {
 
         if (postPwValue.trim() !== '') {
             const passwordError = validatePassword(postPwValue);
-            console.log('postPwValue',postPwValue)
 
             const postData = {
                 password: postPwValue
@@ -111,69 +104,43 @@ const UserInfo = () => {
                 return; // 에러 발생 시 처리 중단
             }
 
-            if(!passwordError) {
-                /*userClient.post('/profile/user/save-pwd', postPwValue)
-                .then(response => {
-                    message.success('패스워드 변경이 성공했습니다.');
-                    // 업로드 후 필요한 작업을 수행합니다.
-                })
-                .catch(error => {
-                    message.error('비밀번호 변경에 실패했습니다.');
-                    // 오류 처리를 수행합니다.
-                    console.log('error', error)
-                });*/
+            if (!passwordError) {
                 dispatch(profileAction.postUserPW(postData));
                 userClient.post('/profile/user/save-pwd', postData)
-                .then(response => {
-                    message.success('성공');
-                    // 업로드 후 필요한 작업을 수행합니다.
-                })
-                .catch(error => {
-                    message.error('실패');
-                    // 오류 처리를 수행합니다.
-                    console.log('error', error)
-                });
+                    .then(response => {
+                        message.success('성공적으로 패스워드를 변경하였습니다.');
+                        setPostPwValue('') // 성공하면 인풋 값 초기화
+                    })
+                    .catch(error => {
+                        message.error('알 수 없는 오류로 변경에 실패하였습니다.');
+                        console.log('error', error) // 에러 로그 보기
+                    });
 
                 // 정규식 추가해야됨
                 // REGEX = "^((?=.\\d)(?=.[a-zA-Z])(?=.*[\\W]).{" + MIN + "," + MAX + "})$"
             }
-            /*if (validatePassword(postPwValue)) {
-                userClient.post('/profile/user/save-pwd', postPwValue)
-                .then(response => {
-                    message.success('패스워드 변경이 성공했습니다.');
-                    // 업로드 후 필요한 작업을 수행합니다.
-                })
-                .catch(error => {
-                    message.error('비밀번호 변경에 실패했습니다.');
-                    // 오류 처리를 수행합니다.
-                    console.log('error', error)
-                });
-            } else {
-                message.error('비밀번호는 영어, 숫자, 특수문자가 각각 하나 이상 포함되어야 하며, 8글자 이상 20글자 이하여야 합니다.');
-            }*/
-            /*if (validatePassword(postPwValue)) {
-                // 유효한 비밀번호일 때 POST 요청 실행
-                /!*dispatch(profileAction.postUserPW(postPwValue));*!/
-                console.log('비밀번호 변경 실행')
-            } else {
-                message.error('비밀번호는 영어, 숫자, 특수문자가 각각 하나 이상 포함되어야 하며, 8글자 이상 20글자 이하여야 합니다.');
-                // 유효하지 않은 비밀번호에 대한 에러 메시지 표시
-            }*/
         }
     };
+
+    const handleSave = () => {
+        onSave();
+    }
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleSave();
+        }
+    } // 엔터 및 클릭시 폼 제출
+
+    const handleCancel = () => {
+        setFileList([]); // 이미지 초기화
+        setPostPwValue(''); // 비밀번호 입력 값 초기화
+    }; // 취소버튼 클릭 시 초기화
 
     ///////////////////////////////////////////////////////////////////////////////////
 
     const [fileList, setFileList] = useState([]);
-
-    const handleUpload = () => {
-
-        /*if (fileList) {
-            console.log('fileList', fileList)
-            dispatch(profileAction.postUserProfileImg(fileList))
-        }*/
-
-    };
 
     const handleFileChange = ({fileList}) => {
         setFileList(fileList);
@@ -184,7 +151,7 @@ const UserInfo = () => {
         beforeUpload: file => {
             if (fileList.length >= 1) {
                 message.warning('이미지는 한 개씩만 업로드할 수 있습니다.');
-                return false;
+                return false; // 두개 이상 올리려고 하면 리턴
             }
             setFileList([...fileList, file]);
             return false; // 업로드 전에 파일 리스트에 추가하고 업로드를 막습니다.
@@ -196,74 +163,65 @@ const UserInfo = () => {
         <>
             <div className={'w-full'}>
 
-                    <div className={'w-fit flex desktop:flex-row gap-[100px] desktop:m-0 desktop:mx-auto'}>
+                <div className={'w-fit flex desktop:flex-row gap-[100px] desktop:m-0 desktop:mx-auto'}>
 
-                        <div className={'flex flex-col items-center '}>
-                            {/*<img src="" alt="#"/>*/}
-                            {userProfileImg == null
-                                ? <Space direction='vertical' wrap size={180}>
-                                    <Avatar size={180} icon={<UserOutlined/>}/>
-                                </Space>
-                                : <img src={userProfileImg} alt="#"
-                                       className={'rounded-full w-[180px] h-full max-w-[180px] max-h-[180px]'}/>
-                            }
+                    <div className={'flex flex-col items-center gap-[8px]'}>
+                        {userProfileImg == null
+                            ? <Space direction='vertical' wrap size={180}>
+                                <Avatar size={180} icon={<UserOutlined/>}/>
+                            </Space>
+                            : <img src={userProfileImg} alt="#"
+                                   className={'rounded-full w-[180px] h-full max-w-[180px] max-h-[180px]'}/>
+                        }
 
-                            <form action="/item" method={'post'} encType={"multipart/form-data"}>
+                        <form action="/item" method={'post'} encType={"multipart/form-data"}>
                             <Upload {...props} >
-                                <Button icon={<UploadOutlined/>}>이미지 업로드</Button>
+                                <Button icon={<UploadOutlined/>}>프로필 변경</Button>
                             </Upload>
-                            <Button onClick={handleUpload}>변경 이미지 저장</Button>
-                            {/*나중에 버튼 ref로 묶어서 하단 저장버튼에서 함수 실행되도록 바꾸기 */}
-                            </form>
+                        </form>
+                    </div>
+                    <div className={'flex flex-col gap-[6px]'}>
+                        <div
+                            className={'flex desktop:min-w-[80px] gap-[32px] font-[NotoSansKR-500] text-[16px] text-[#51525c] '}>
+                            <p className={'desktop:min-w-[89px]'}>아이디</p>
+                            <p className={''}>{fullUserInfo?.userInfo?.userEmail}</p>
                         </div>
-                        <div className={'flex flex-col gap-[6px]'}>
-                            <div
-                                className={'flex desktop:min-w-[80px] gap-[32px] font-[NotoSansKR-500] text-[16px] text-[#51525c] '}>
-                                <p className={'desktop:min-w-[80px]'}>아이디</p>
-                                <p className={''}>{fullUserInfo?.userInfo.userEmail}</p>
-                            </div>
 
+                        <div
+                            className={'flex desktop:min-w-[80px] gap-[32px] font-[NotoSansKR-500] text-[16px] text-[#51525c] '}>
+                            <p className={'desktop:min-w-[89px]'}>이름</p>
+                            <p>{fullUserInfo?.userInfo?.userNm}</p>
+                        </div>
+                        <div
+                            className={'flex desktop:min-w-[80px] gap-[32px] font-[NotoSansKR-500] text-[16px] text-[#51525c] '}>
+                            <p className={'desktop:min-w-[89px]'}>핸드폰 번호</p>
+                            <p>{fullUserInfo?.userInfo?.phoneNumber}</p>
+                        </div>
+                        <form action=""
+                              method={'post'}
+                              encType={"application/json"}
+                        >
                             <div
-                                className={'flex desktop:min-w-[80px] gap-[32px] font-[NotoSansKR-500] text-[16px] text-[#51525c] '}>
-                                <p className={'desktop:min-w-[80px]'}>이름</p>
-                                <p>{fullUserInfo?.userInfo.userNm}</p>
-                            </div>
-                            <div
-                                className={'flex desktop:min-w-[80px] gap-[32px] font-[NotoSansKR-500] text-[16px] text-[#51525c] '}>
-                                <p className={'desktop:min-w-[80px]'}>핸드폰 번호</p>
-                                <p>{fullUserInfo?.userInfo.phoneNumber}</p>
-                            </div>
-                            <form action=""
-                                  method={'post'}
-                                  encType={"application/json"}
-                            >
+                                className={'flex gap-[32px] font-[NotoSansKR-500] text-[16px] text-[#51525c] items-center '}>
                                 <div
-                                    className={'flex gap-[32px] font-[NotoSansKR-500] text-[16px] text-[#51525c] items-center '}>
-                                    <div
-                                        className={'h-fit desktop:h-[35.141px] desktop:max-h-[35.141px] self-start flex items-center'}>
-                                        <p className={'desktop:min-w-[80px]'}>비밀번호변경</p>
-                                    </div>
-                                    <div className={'flex flex-col desktop:flex-col gap-[6px]'}>
-                                        {/*<button
-                                        className={'box-border bg-[#E3E4E8] text-[#51525c] px-[10px] py-[5px] rounded-[5px] min-w-fit w-fit desktop:max-w-[141.8px]'}
-                                        onClick={() => setPasswordInput(!passwordInput)}>비밀번호 변경하기
-                                    </button>*/}
-                                        {/*{passwordInput && }*/}
-                                        <SSinput className={'mb-[0]  desktop:max-w-[141.8px]'}
-                                                 onChange={(e) => setPostPwValue(e.target.value)}
-                                        />
-                                        {/*input value값 post 요청하기 - 핸들러는 저장버튼 눌렀을 때 요청*/}
-                                    </div>
+                                    className={'h-fit desktop:h-[35.141px] desktop:max-h-[35.141px] self-start flex items-center'}>
+                                    <p className={'desktop:min-w-[89px]'}>비밀번호변경</p>
                                 </div>
-                            </form>
-                        </div>
+                                <div className={'flex flex-col desktop:flex-col gap-[6px]'}>
+                                    <SSinput className={'mb-[0]  desktop:max-w-[141.8px]'}
+                                             onChange={(e) => setPostPwValue(e.target.value)}
+                                    />
+                                </div>
+                            </div>
+                        </form>
                     </div>
+                </div>
 
-                    <div
-                        className={'w-full flex justify-center items-center gap-[16px] mt-[20px] tablet:mt-[40px] desktop:mt-[80px] '}>
-                        <SSbutton danger className={'px-[60px]'}>취소</SSbutton>
-                        <SSbutton type={'primary'} className={'px-[60px]'} onClick={onSave}>저장</SSbutton>
-                    </div>
+                <div
+                    className={'w-full flex justify-center items-center gap-[16px] mt-[20px] tablet:mt-[40px] desktop:mt-[80px] '}>
+                    <SSbutton danger className={'px-[60px]'} onClick={handleCancel}>취소</SSbutton>
+                    <SSbutton type={'primary'} className={'px-[60px]'} onClick={handleSave}>저장</SSbutton>
+                </div>
 
             </div>
         </>
