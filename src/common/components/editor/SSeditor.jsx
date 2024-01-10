6bb76c872d337dab1a529a627e301f154fbd9fab
unknown <41793830+ravenkim@users.noreply.github.com>
2024-01-10 09:33:00 +0900

    소니 부활

--- src/common/components/editor/SSeditor.jsx
+++ src/common/components/editor/SSeditor.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
-// import SunEditor from 'suneditor-react'
-// import 'suneditor/dist/css/suneditor.min.css'
-// import ko from 'suneditor/src/lang/ko.js'
-// import {LIME_EDITOR_OPTIONS} from "./EditorConstants.jsx"
+import SunEditor from 'suneditor-react'
+import 'suneditor/dist/css/suneditor.min.css'
+import ko from 'suneditor/src/lang/ko.js'
+import {LIME_EDITOR_OPTIONS} from "./EditorConstants.jsx"
 
 const SSeditor = ({height = "500px", autoFocus, isEditMode, initContents, changeHandler, customOptions}) => {
     return (
-    <></>
-        // <SunEditor
-        //     lang={ko}
-        //     autoFocus={autoFocus}
-        //     disable={!isEditMode}
-        //     hideToolbar={!isEditMode}
-        //     setContents={initContents}
-        //     height={height}
-        //     setDefaultStyle={`font-family: 'Malgun Gothic'; font-size: 14px;`}
-        //     onChange={(contents) => {
-        //         if (changeHandler && typeof changeHandler === "function") {
-        //             changeHandler(contents);
-        //         }
-        //     }}
-        //     setOptions={{
-        //         ...LIME_EDITOR_OPTIONS,
-        //         ...customOptions,
-        //     }}
-        // />
+        <SunEditor
+            lang={ko}
+            autoFocus={autoFocus}
+            disable={!isEditMode}
+            hideToolbar={!isEditMode}
+            setContents={initContents}
+            height={height}
+            setDefaultStyle={`font-family: 'Malgun Gothic'; font-size: 14px;`}
+            onChange={(contents) => {
+                if (changeHandler && typeof changeHandler === "function") {
+                    changeHandler(contents);
+                }
+            }}
+            setOptions={{
+                ...LIME_EDITOR_OPTIONS,
+                ...customOptions,
+            }}
+        />
+
     );
 };
 
