36f74410ff7897a6e1e397e235b417f00afb6487
unknown <41793830+ravenkim@users.noreply.github.com>
2024-01-22 11:34:13 +0900

    글자색 추가

--- src/common/utils/colorUtils.jsx
+++ src/common/utils/colorUtils.jsx
@@ -11,3 +11,13 @@ export const generateColorCode = str => {
     }
     return color;
 }
+
+
+export  const  generateTextColorCode = (hexcolor) => {
+    hexcolor = hexcolor.replace("#", "");
+    const r = parseInt(hexcolor.substr(0, 2), 16);
+    const g = parseInt(hexcolor.substr(2, 2), 16);
+    const b = parseInt(hexcolor.substr(4, 2), 16);
+    const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+    return (yiq >= 128) ? 'black' : 'white';
+}
