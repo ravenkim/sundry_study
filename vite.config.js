615f6d3d90f1134a5c8a1f017b5557cba0810074
ravenkim <41793830+ravenkim@users.noreply.github.com>
2024-01-26 10:56:14 +0900

    [성능개선] 택스트 압축

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { compression } from 'vite-plugin-compression2'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		compression()
+	],
 
 	//경로 확인
 	resolve: {
