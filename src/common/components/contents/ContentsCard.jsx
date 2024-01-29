031a403c80bd56a134ba147d213d3b6bbb519f8b
cmpark <cksals123@gmail.com>
2024-01-29 10:41:42 +0900

    [수정] 레이아웃 / 카드 스타일 변경

--- src/common/components/contents/ContentsCard.jsx
+++ src/common/components/contents/ContentsCard.jsx
@@ -1,14 +1,19 @@
+import {useState} from "react";
+import UnLikedHeart from 'src/assets/img/heart.svg'
+import LikeHeart from 'src/assets/img/fill_heart.svg'
 
 const ContentsCard = ({
-    idx,
-    item,
-    onClick,
-    ...otherProps
+                          idx,
+                          item,
+                          onClick,
+                          ...otherProps
                       }) => {
+
+    const [likeStatus, setLikeStatus] = useState(false)
 
     return (
         <>
-            <div key={item?.contentId}
+            {/*<div key={item?.contentId}
                  className={"group relative rounded-[10px] overflow-hidden max-w-[188px] desktop:w-[188px] desktop:h-[188px] transition-all cursor-pointer bg-[#ffffff] shadow-[0px_1px_4px_rgba(0,0,0,0.3)] flex justify-center items-center " +
                      "w-[188px] h-[188px]"}
                  onClick={onClick}
@@ -30,6 +35,39 @@ const ContentsCard = ({
                     <div className={'w-full flex flex-row justify-between items-end'}>
                         <div className={'flex flex-row gap-[6px] line-clamp-1 flex-auto flex-nowrap'}>
                             <span className={'text-[#51525C] text-[9px] box-border bg-[#ffffff] px-[7px] py-[3px] rounded-[10px] w-fit min-w-fit'}>{item?.cateNm}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>*/}
+
+
+            <div key={item?.contentId}
+                 className={'group relative flex-auto w-[23%] max-w-[23.5%] rounded-[10px] overflow-hidden transition-all cursor-pointer shadow-[0px_1px_4px_rgba(0,0,0,0.3)] hover:-translate-y-1 '}
+            >
+                {/*이미지 박스*/}
+                <div className={'w-full h-[150px] max-h-[150px] flex justify-center items-center overflow-hidden '}>
+                    <img src={item?.url} alt="#" className={'w-full h-auto'}/>
+                </div>
+
+                {/*텍스트 박스*/}
+                <div className={'box-border p-[8px] flex flex-col justify-between min-h-[140px]'}>
+                    <div>
+                        <h5 className={'text-[#232433] text-[18px] line-clamp-2 font-[NotoSansKR-700] break-keep mb-[3px]'}>{item?.contentNm}</h5>
+                        <p className={'text-[#51525C] text-[12px] font-[NotoSansKR-300] line-clamp-2'}>{item?.contentDesc}</p>
+                    </div>
+                    <div className={'flex justify-between'}>
+                        <div>
+                            <span
+                                className={'text-[#51525C] text-[9px] box-border bg-[#ffffff] px-[7px] py-[3px] rounded-[10px] w-fit min-w-fit border-[1px] border-[#E3E4E8] border-solid'}>{item?.cateNm}</span>
+                        </div>
+                        <div className={'flex gap-[4px]'}>
+                            <img src={likeStatus ? LikeHeart : UnLikedHeart}
+                                 alt="좋아요"
+                                 className={'transition-all hover:scale-125 relative z-2'}
+                                 onClick={() => setLikeStatus(!likeStatus)}
+
+                            />
+                            <span className={''}>예약</span>
                         </div>
                     </div>
                 </div>
--- src/common/components/wrapper/SSsectionWrap.jsx
+++ src/common/components/wrapper/SSsectionWrap.jsx
@@ -1,7 +1,7 @@
 const SSsectionWrap = ({children, className, ...otherprops}) => {
     return (
         <section
-            className={`m-0 mx-auto w-full desktop:max-w-[800px] px-[20px] py-[50px] tablet:px-[60px] tablet:py-[80px] desktop:px-[0px] flex flex-col gap-[40px] tablet:gap-[60px] ` + className}
+            className={`m-0 mx-auto w-full desktop:max-w-[1000px] px-[20px] py-[50px] tablet:px-[60px] tablet:py-[80px] desktop:px-[0px] flex flex-col gap-[40px] tablet:gap-[60px] ` + className}
             {...otherprops}
         >
             {children}
