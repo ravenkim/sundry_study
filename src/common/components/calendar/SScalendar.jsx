20f5748f1cd779aa07575eba78628a8e43523e81
cmpark <cksals123@gmail.com>
2024-02-19 13:46:55 +0900

    [추가] 빅캘린더 나오는지 확인하기 위해 추가

---
+++ src/common/components/calendar/SScalendar.jsx
@@ -0,0 +1,153 @@
+import React, {useState} from 'react';
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const Toolbar = ({date}) => {
+    const navigate = (action) => {
+        date.onNavigate(action);
+    }
+
+    return (
+        <div className="rbc-toolbar">
+      <span className="rbc-btn-group">
+        <button type="button" onClick={navigate.bind(null, 'TODAY')}>
+          이번달
+        </button>
+        <button
+            type="button"
+            onClick={navigate.bind(null, 'PREV')}
+        >
+          이전
+        </button>
+        <span className="rbc-toolbar-label">{`${date.getFullYear()}년 ${date.getMonth() + 1}월`}</span>
+        <button
+            type="button"
+            onClick={navigate.bind(null, 'NEXT')}
+        >
+          다음
+        </button>
+      </span>
+        </div>
+    )
+}
+
+const SScalendar = () => {
+    moment.locale('ko-KR'); // 시간 국가 설정 // https://github.com/moment/moment/tree/develop/locale
+    const localizer = momentLocalizer(moment) // 시간 생성
+
+    const [myEventsList, setMyEventsList] = useState()
+
+    return (
+        <>
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                view={'month'}
+                components={{
+                    toolbar: Toolbar,
+                }}
+                resizable
+                selectable
+
+                className={'w-full min-h-[400px] h-auto'} // 스타일 정의
+            />
+        </>
+    );
+};
+
+export default SScalendar;
+
+
+const events = [
+  {
+    'title': 'All Day Event very long title',
+    'allDay': true,
+    'start': new Date(2024, 1, 1), // monthIndex는 0부터 1월
+    'end': new Date(2024, 1, 10) // end date는 -1값이 실제 표시값
+  },
+  {
+    'title': 'Long Event',
+    'start': new Date(2024, 1, 7),
+    'end': new Date(2024, 1, 10)
+  },
+
+  {
+    'title': 'DTS STARTS',
+    'start': new Date(2024, 1, 13, 0, 0, 0),
+    'end': new Date(2024, 1, 20, 0, 0, 0)
+  },
+
+  {
+    'title': 'DTS ENDS',
+    'start': new Date(2024, 1, 6, 0, 0, 0),
+    'end': new Date(2024, 1, 13, 0, 0, 0)
+  },
+
+  {
+    'title': 'Some Event',
+    'start': new Date(2024, 1, 9, 0, 0, 0),
+    'end': new Date(2024, 1, 9, 0, 0, 0)
+  },
+  {
+    'title': 'Conference',
+    'start': new Date(2024, 3, 11),
+    'end': new Date(2024, 3, 13),
+    desc: 'Big conference for important people'
+  },
+  {
+    'title': 'Meeting',
+    'start': new Date(2024, 3, 12, 10, 30, 0, 0),
+    'end': new Date(2024, 3, 12, 12, 30, 0, 0),
+    desc: 'Pre-meeting meeting, to prepare for the meeting'
+  },
+  {
+    'title': 'Lunch',
+    'start': new Date(2024, 3, 12, 12, 0, 0, 0),
+    'end': new Date(2024, 3, 12, 13, 0, 0, 0),
+    desc: 'Power lunch'
+  },
+  {
+    'title': 'Meeting',
+    'start': new Date(2024, 3, 12, 14, 0, 0, 0),
+    'end': new Date(2024, 3, 12, 15, 0, 0, 0)
+  },
+  {
+    'title': 'Happy Hour',
+    'start': new Date(2024, 2, 12, 17, 0, 0, 0),
+    'end': new Date(2024, 2, 12, 17, 30, 0, 0),
+    desc: 'Most important meal of the day'
+  },
+  {
+    'title': 'Dinner',
+    'start': new Date(2024, 3, 12, 20, 0, 0, 0),
+    'end': new Date(2024, 3, 12, 21, 0, 0, 0)
+  },
+  {
+    'title': 'Birthday Party',
+    'start': new Date(2015, 3, 13, 7, 0, 0),
+    'end': new Date(2015, 3, 13, 10, 30, 0)
+  },
+  {
+    'title': 'Birthday Party 2',
+    'start': new Date(2015, 3, 13, 7, 0, 0),
+    'end': new Date(2015, 3, 13, 10, 30, 0)
+  },
+  {
+    'title': 'Birthday Party 3',
+    'start': new Date(2015, 3, 13, 7, 0, 0),
+    'end': new Date(2015, 3, 13, 10, 30, 0)
+  },
+  {
+    'title': 'Late Night Event',
+    'start': new Date(2015, 3, 17, 19, 30, 0),
+    'end': new Date(2015, 3, 18, 2, 0, 0)
+  },
+  {
+    'title': 'Multi-day Event',
+    'start': new Date(2015, 3, 20, 19, 30, 0),
+    'end': new Date(2015, 3, 22, 2, 0, 0)
+  }
+]
---
+++ src/common/components/calendar/SScalendarWarp.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import SSmodal from "../modal/SSmodal.jsx";
+import SScalendar from "./SScalendar.jsx";
+
+const SScalendarWarp = ({calendarVisible,setCalendarVisible}) => {
+
+    const onCancel = () => {
+        setCalendarVisible(!calendarVisible)
+    }
+
+    const onOk = () => {
+        setCalendarVisible(!calendarVisible)
+    }
+
+    return (
+        <>
+            <SSmodal
+                title={'예약 일정'}
+                visible={calendarVisible}
+                onCancel={onCancel}
+                onOk={onOk}
+                okText={'확인'}
+                cancelText={'취소'}
+            >
+                <SScalendar/>
+            </SSmodal>
+        </>
+    );
+};
+
+export default SScalendarWarp;
