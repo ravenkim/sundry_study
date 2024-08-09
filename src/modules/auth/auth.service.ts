6ead1457e954875c76b82e083ee2b09c916d54b0
ravenkim <41793830+ravenkim@users.noreply.github.com>
2024-08-09 15:29:37 +0900

    jwt 추가

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common'
 import { DatabaseService } from '../../database/database.service'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly databaseService: DatabaseService) {
-    }
+    constructor(
+        private readonly databaseService: DatabaseService,
+        private jwtService: JwtService
+    ) {}
 
     async login() {
+
+        //비밀번호 일치 여부 확인
+
+
+
+
+
+        const payload = {
+
+        }
+
         //토큰 만들어서 넘겨주기
-        const accessToken = ''
+        const accessToken = await this.jwtService.signAsync(payload),
         const refreshToken = ''
 
         return {
