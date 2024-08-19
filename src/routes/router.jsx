5f148b148ca885bb7f7ed0ed61558bdb082d1e0d
ravenkim <ravenkim97@naver.com>
2024-08-19 20:15:57 +0900

    홈추가

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router-dom'
 import Test2 from 'src/pages/Test2.jsx'
 import Test1 from 'src/pages/Test1.jsx'
 import ErrorPage from 'src/pages/ErrorPage/ErrorPage.jsx'
-import SamplePage from 'src/pages/SamplePage.jsx'
 import LoginPage from 'src/pages/login/LoginPage.jsx'
 import HomePage from 'src/pages/home/HomePage.jsx'
 import MainPage from 'src/pages/main/MainPage.jsx'
@@ -17,6 +16,11 @@ export const router = createBrowserRouter([
     {
         path: '/login',
         element: <LoginPage />,
+    },
+
+    {
+        path: '/home',
+        element: <HomePage />,
     },
 
     {
