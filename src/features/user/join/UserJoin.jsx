8fdc7670f339e93093202524b98dac213ccfaaca
ravenkim <41793830+ravenkim@users.noreply.github.com>
2024-09-10 13:44:47 +0900

    가이드라인 추가

---
+++ src/features/user/join/UserJoin.jsx
@@ -0,0 +1,27 @@
+
+const UserJoin = () => {
+    return (
+
+
+        // 회원가입
+
+
+        // 순차적으로
+        // 1. intro
+        // 2. 계정생성
+        // 3. 본인 확인
+        // 4. 로그인 페이지로
+
+
+
+
+
+
+
+        <div>
+
+        </div>
+    )
+}
+
+export default UserJoin
