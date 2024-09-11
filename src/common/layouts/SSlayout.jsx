db9faa6b0b3a27dafa6658ae12f92331bad8d33d
ravenkim <41793830+ravenkim@users.noreply.github.com>
2024-09-11 16:51:21 +0900

    modify: [nav] 주석 추가

--- src/common/layouts/SSlayout.jsx
+++ src/common/layouts/SSlayout.jsx
@@ -3,6 +3,7 @@ import NavigationBar from 'src/common/layouts/NavigationBar.jsx'
 
 const SSlayout = ({ layoutType = 'mobile', children, useNav = true }) => {
     const navBarSize = 68
+    // todo 이거 디자인 맞게 수정시 아이콘 크기, 패딩도 수정 필요
 
 
     // todo 500 까지는 모바일, 일단 500보다 커도 배경 나오게끔 설정함 향후 큰 화면 개발시 레이아웃 수정 필요
