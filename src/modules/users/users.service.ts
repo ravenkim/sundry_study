0ea1acefd353919e542d645f1917e7070dc50c05
ravenkim <41793830+ravenkim@users.noreply.github.com>
2024-11-07 16:23:49 +0900

    feat: todo 기능 추가

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -36,28 +36,59 @@ export class UsersService {
         }
     }
 
+    // 비밀번호 형식이 올바른지 확인
+    //ttt 이러면 어찌됬는 비밀번호가 서버측에서 남는거 아닌가? 프런트 에서 부터 암호화 해서 들고 와야 하는지 그렇다면 어찌 비밀번호 형식을 검증하는지
+    async checkValidPassword(password: string) {
+        const minLength = 8
+        const hasUpperCase = /[A-Z]/.test(password)
+        const hasLowerCase = /[a-z]/.test(password)
+        const hasNumber = /[0-9]/.test(password)
+        const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+
+        if (
+            password.length < minLength ||
+            !hasUpperCase ||
+            !hasLowerCase ||
+            !hasNumber ||
+            !hasSpecialChar
+        ) {
+            throw new BadRequestException('잘못된 비번입니다.')
+        }
+    }
+
     //
     /* todo
-
-    1. id 중복 체크
-    
-    
-    
-    
-
-
-    2. id 와 비밀번호를 저장
-    -id 중복체크
-    -비밀번호 해쉬화
-    저장
-
-
     본인인증
     3. 이름, 이메일, 이메일 광고 수신여부, 생년월일 , 전화번호 입력받음
-
-
-
      */
+
+    // 계정 만들기
+    async createAccount(request: CreateAccountRequestDto) {
+        const { loginId, email, password } = request
+
+        //중복 여부 확인
+        //ttt 아래의 두개의 검증은 디비를 두번 탑니다. 한번에 타게끔 하는 함수를 따로 만들어야 할까요?
+        //ttt 아래의 두개의 함수는 컨트롤로 옴기는게 맞나요?
+        await this.checkLoginIdDuplicate(loginId)
+        await this.checkEmailDuplicate(email)
+
+        // 아이디 생성
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = await this.prisma.user.create({
+            data: {
+                login_id: loginId,
+                email: email,
+                password: hashedPassword,
+            },
+            //todo 만들어진 타입 노말 추가
+        })
+
+        console.log(newUser)
+        // todo newUser.id 를 리턴해주고 나중에 id 업데이트
+
+        return '성공적으로 만들어짐'
+    }
 
     async cancelCreateUser() {
         // 본인인증 완료가 안된 아이디 로 가입하다가 나간경우 아이디 삭제
@@ -105,26 +136,6 @@ export class UsersService {
     //***************************************회원가입****************************************************//
 
     // 아이디 중복 확인
-
-    // 계정 만들기
-    async createAccount(request: CreateAccountRequestDto) {
-        const { loginId, email, password } = request
-
-        //중복 여부 확인
-        //ttt 아래의 두개의 검증은 디비를 두번 탑니다. 한번에 타게끔 하는 함수를 따로 만들어야 할까요?
-        await this.checkLoginIdDuplicate(loginId)
-        await this.checkEmailDuplicate(email)
-
-        // 아이디 생성
-        // const hashedPassword = await bcrypt.hash(userPassword, 10)
-        //
-        // const result = await this.databaseService.query(
-        //     'src/modules/users/sql/create_account.sql',
-        //     [userLoginId, hashedPassword, userEmail, 'normal'],
-        // )
-
-        return '성공적으로 만들어짐'
-    }
 
     async checkUserAuthentication() {
         //todo 전화번호 인증으로 변경 예정
