4deac586930f2fa4a8fee9fd2a58c647c2f5593a
unhhyyeexx <unhhyyee66@gmail.com>
2024-11-11 22:31:53 +0900

    feat: [로그인] 로그인화면 구성

--- src/common/components/input/SSinput.jsx
+++ src/common/components/input/SSinput.jsx
@@ -2,27 +2,20 @@ import { Input } from 'src/assets/shadcn/components/ui/input.jsx'
 import { useCallback, useEffect, useId, useState } from 'react'
 import debounce from 'lodash.debounce'
 
-
-
 // todo import { z } from "zod" 추가해서 검증 로직 추가 할것
 
-
-
-
 const SSinput = ({
-                     value,
-                     onChange,
-                     description,
-                     label,
-                     className,
-                     type = 'text',
-                     onKeyDown,
-                 }) => {
-
-
+    placeholder,
+    value,
+    onChange,
+    description,
+    label,
+    className,
+    type = 'text',
+    onKeyDown,
+}) => {
     //고유 id 생성
     const inputId = useId()
-
 
     // 입력 필드의 값
     const [inputValue, setInputValue] = useState(value)
@@ -37,9 +30,6 @@ const SSinput = ({
         setTextLength(inputValue)
     }, [inputValue])
 
-
-
-
     //포커스 여부 관리
     const [isFocused, setIsFocused] = useState(false)
     const onFocus = () => {
@@ -49,17 +39,18 @@ const SSinput = ({
     const onBlur = () => {
         setIsFocused(false)
         debouncedOnChange.cancel()
-        onChange(inputValue)
+        // onChange(inputValue)
+        setInputValue(inputValue)
     }
 
     // onChange 디바운스 처리
     const debouncedOnChange = useCallback(
         debounce((value) => {
-            onChange(value)
+            // onChange(value)
+            setInputValue(value)
         }, 300),
-        [onChange]
+        [onChange],
     )
-
 
     useEffect(() => {
         if (isFocused) {
@@ -67,11 +58,11 @@ const SSinput = ({
         }
     }, [inputValue, debouncedOnChange, isFocused])
 
-
     // 버튼 누르면 디바운스 멈추고 동작
     const handleKeyDown = (event) => {
         debouncedOnChange.cancel()
-        onChange(inputValue)
+        // onChange(inputValue)
+        setInputValue(inputValue)
         if (onKeyDown) {
             onKeyDown(event)
         }
@@ -84,7 +75,7 @@ const SSinput = ({
         <div className={`w-full`}>
             <label htmlFor={inputId}>{label}</label>
             <Input
-                className={` ${className}`}
+                className={`h-[44px] border-muted ${className}`}
                 id={inputId}
                 type={type}
                 value={inputValue}
@@ -92,6 +83,7 @@ const SSinput = ({
                 onBlur={onBlur}
                 onChange={(e) => setInputValue(e.target.value)}
                 onKeyDown={handleKeyDown}
+                placeholder={placeholder}
             />
             {isError ? <>{errorMessage}</> : <> {description}</>}
         </div>
--- src/features/login/Login.jsx
+++ src/features/login/Login.jsx
@@ -1,27 +1,33 @@
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 import SSinnerWrapper from 'src/common/components/wrapper/SSinnerWrapper.jsx'
 import SStopBar from 'src/common/components/topBar/SStopBar.jsx'
-import LogoWithTitle from 'src/assets/images/logo/logoWithTitle.svg?react'
-import SSinput from 'src/features/sample/Taaa.jsx'
+import SSinput from 'src/common/components/input/SSinput'
 import SSbutton from 'src/common/components/button/SSbutton.jsx'
-import { useNavigate } from 'react-router-dom'
+import LogoWithTitle from 'src/assets/images/logo/logoWithTitle.svg?react'
+import IconEye from 'src/assets/images/icon/Icon_eye.svg?react'
+import IconEyeOff from 'src/assets/images/icon/Icon_eyeOff.svg?react'
+import IconCheck from 'src/assets/images/icon/Icon_check.svg?react'
 
 const Login = () => {
     /*
+     * todo
      * 1. 로그인 여부 확인
      * 2. 비로그인시 로그인 페이지
      * 3. 로그인시 상세정보 yn 체크후
      * 4. n 이면 상세정보 입력
      * 5. y면 홈으로 이동
      *
+     * 6. 로그인 유지 기능 추가
      * */
 
-    const { isLoggedIn } = useSelector(
-        ({ authReducer }) => ({
-            isLoggedIn: authReducer.isLoggedIn.data,
-        }),
-        shallowEqual,
-    )
+    // const { isLoggedIn } = useSelector(
+    //     ({ authReducer }) => ({
+    //         isLoggedIn: authReducer.isLoggedIn.data,
+    //     }),
+    //     shallowEqual,
+    // )
 
     // 로그아웃은 마이페이지 에서
 
@@ -30,6 +36,16 @@ const Login = () => {
     const loginHandler = (id, password) => {}
 
     const navigate = useNavigate()
+
+    const [isShowPw, setShowPw] = useState(false)
+    const handleShowPwChecked = () => {
+        setShowPw(!isShowPw)
+    }
+
+    const [isKeepLogin, setKeepLogin] = useState(false)
+    const handleKeepLogin = () => {
+        setKeepLogin(!isKeepLogin)
+    }
 
     return (
         <SSinnerWrapper>
@@ -42,10 +58,84 @@ const Login = () => {
                 <LogoWithTitle />
             </div>
 
-            <SSinput className={`mb-[8px]`} />
-            <SSinput className={`mb-[16px]`} type={`password`} />
+            <SSinput className={`mb-[8px]`} placeholder={'아이디'} />
+            <div className="flex-grow">
+                <div className="relative w-full">
+                    <SSinput
+                        className={`mb-[16px]`}
+                        placeholder={'비밀번호'}
+                        type={isShowPw ? `text` : `password`}
+                    />
+                    {isShowPw ? (
+                        <div className="absolute right-3 top-2 h-6 w-6 bg-white">
+                            <IconEye
+                                className="stroke-muted"
+                                onClick={handleShowPwChecked}
+                            />
+                        </div>
+                    ) : (
+                        <div className="absolute right-3 top-2 h-6 w-6 bg-white">
+                            <IconEyeOff
+                                className="stroke-muted"
+                                onClick={handleShowPwChecked}
+                            />
+                        </div>
+                    )}
+                </div>
+            </div>
 
-            <SSbutton text={'로그인'} />
+            {/* 로그인 유지 toggle */}
+            <div>
+                <label className="relative flex">
+                    <input
+                        type="checkbox"
+                        className="absolute m-[2px] w-0 overflow-hidden"
+                        onChange={handleKeepLogin}
+                    />
+                    <div className="mb-4 flex">
+                        <div
+                            className={`mr-2 flex h-[16px] w-[16px] items-center justify-center rounded-sm ${isKeepLogin ? 'bg-border' : 'bg-muted'}`}
+                        >
+                            <IconCheck />
+                        </div>
+                        <span className="flex items-center text-input">
+                            로그인 상태 유지하기
+                        </span>
+                    </div>
+                </label>
+            </div>
+
+            {/* todo 로그인 기능 추가 */}
+            <SSbutton
+                className={'h-[48px]'}
+                text={'로그인'}
+                onClick={loginHandler}
+            />
+
+            {/* 아이디, 비번 찾기, 회원가입 */}
+            {/* todo 링크 걸기 */}
+            <section className="flex w-full items-center justify-center">
+                <span
+                    className="text-sm text-muted-foreground"
+                    onClick={() => navigate('/findId')}
+                >
+                    아이디 찾기
+                </span>
+                <span className="m-2 h-[12px] border-[1px] border-muted"></span>
+                <span
+                    className="text-sm text-muted-foreground"
+                    onClick={() => navigate('/findPwd')}
+                >
+                    비밀번호 찾기
+                </span>
+                <span className="m-2 h-[12px] border-[1px] border-muted"></span>
+                <span
+                    className="text-sm text-muted-foreground"
+                    onClick={() => navigate('/join')}
+                >
+                    회원가입
+                </span>
+            </section>
         </SSinnerWrapper>
     )
 }
