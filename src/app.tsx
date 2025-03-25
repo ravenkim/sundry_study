2bf7838ff5f03d0bc93e774f7431155fe8ea3838
Sahaj Jain <sahajjain13@gmail.com>
2025-03-26 03:49:38 +0530

    fix: Add back posthog

--- src/app.tsx
+++ src/app.tsx
@@ -7,24 +7,30 @@ import Index from "./pages/index";
 import Marketing from "./pages/marketing";
 import NotFound from "./pages/not-found";
 import { ThemeProvider } from "./components/theme-provider";
+import { PostHogProvider } from "posthog-js/react";
 
 const queryClient = new QueryClient();
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+}
 
 const App = () => (
   <ThemeProvider defaultTheme="light">
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Marketing />} />
-            <Route path="/editor/:editorType" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Marketing />} />
+              <Route path="/editor/:editorType" element={<Index />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </PostHogProvider>
   </ThemeProvider>
 );
 
