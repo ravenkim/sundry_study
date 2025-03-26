01c1452b5d9405fa511269dfe3030226c5c1caeb
Sahaj Jain <sahajjain13@gmail.com>
2025-03-26 23:14:10 +0530

    feat: Add mail component

---
+++ src/components/examples/mail/index.tsx
@@ -0,0 +1,13 @@
+import { Mail } from "@/components/examples/mail/components/mail";
+import { accounts, mails } from "@/components/examples/mail/data";
+
+export default function MailPage() {
+  return (
+    <Mail
+      accounts={accounts}
+      mails={mails}
+      navCollapsedSize={4}
+      defaultLayout={[]}
+    />
+  );
+}
