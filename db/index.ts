dee79324acaac8dbdb61f28fa6134e335bef0d23
Luis Llanes <luisllaboj@gmail.com>
2025-04-29 13:56:40 -0700

    chore: add dotenv package to db config to read local env file

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,9 @@
+import "dotenv/config";
+import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+
+config({ path: ".env.local" });
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql });
