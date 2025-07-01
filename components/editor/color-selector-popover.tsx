19bdf25f0696da532630e2910feefc9a9a69d7f6
Sahaj Jain <sahajjain13@gmail.com>
2025-07-01 13:45:39 +0530

    update UI for color select popover

--- components/editor/color-selector-popover.tsx
+++ components/editor/color-selector-popover.tsx
@@ -15,9 +15,10 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { cn } from "@/lib/utils";
 import { TAILWIND_PALETTE } from "@/utils/registry/tailwind-colors";
-import { Check } from "lucide-react";
+import { Check, LayoutGrid, LayoutList, List } from "lucide-react";
 import { useCallback } from "react";
 import { Separator } from "../ui/separator";
+import { debounce } from "@/utils/debounce";
 
 type ColorSelectorPopoverProps = {
   currentColor: string;
@@ -49,7 +50,10 @@ export function ColorSelectorPopover({
     <Popover>
       <PopoverTrigger asChild>
         <TooltipWrapper asChild label="Tailwind Colors">
-          <Button variant="ghost" size="icon" className="group">
+          <Button
+            variant="ghost"
+            className="group shadown-none bg-input bg-input/25 border-border/20 size-8 rounded border"
+          >
             <TailwindCSS className="text-foreground group-hover:text-accent-foreground size-4 transition-colors" />
           </Button>
         </TooltipWrapper>
@@ -60,15 +64,21 @@ export function ColorSelectorPopover({
           <div className="flex items-center justify-between gap-4">
             <div className="ml-2 flex items-center gap-1.5">
               <TailwindCSS className="size-4" />
-              <span className="text-muted-foreground text-sm tabular-nums">v4</span>
+              <span className="text-muted-foreground text-sm tabular-nums">Tailwind v4</span>
             </div>
 
             <TabsList className="bg-transparent">
-              <TabsTrigger value="list" className="data-[state=active]:shadow-none">
-                List
+              <TabsTrigger
+                value="list"
+                className="data-[state=active]:bg-input/25 data-[state=active]:shadow-none"
+              >
+                <List className="size-4" />
               </TabsTrigger>
-              <TabsTrigger value="palette" className="data-[state=active]:shadow-none">
-                Palette
+              <TabsTrigger
+                value="palette"
+                className="data-[state=active]:bg-input/25 data-[state=active]:shadow-none"
+              >
+                <LayoutGrid className="size-4" />
               </TabsTrigger>
             </TabsList>
           </div>
@@ -76,7 +86,7 @@ export function ColorSelectorPopover({
 
           <TabsContent value="list" className="my-0 min-w-[300px]">
             <Command className="flex h-84 flex-col">
-              <CommandInput placeholder="Search Tailwind colors..." />
+              <CommandInput className="h-10" placeholder="Search Tailwind colors..." />
               <ScrollArea className="flex-1 overflow-hidden">
                 <CommandEmpty className="text-muted-foreground p-4 text-center">
                   No Tailwind color found.
@@ -99,6 +109,7 @@ export function ColorSelectorPopover({
                               color={color}
                               name={`${key}-${shade}`}
                               isSelected={isSelected}
+                              size="md"
                             />
                             <span>{`${key}-${shade}`}</span>
                             {isSelected && <Check className="ml-auto size-4 opacity-70" />}
@@ -114,10 +125,10 @@ export function ColorSelectorPopover({
 
           <TabsContent value="palette" className="my-0 w-full">
             <ScrollArea className="h-84 w-full">
-              <div className="flex flex-col gap-2 p-3">
+              <div className="flex flex-col gap-0.5 p-1">
                 {Object.entries(TAILWIND_PALETTE).map(([key, colors]) => {
                   return (
-                    <div key={key} className="flex gap-2">
+                    <div key={key} className="flex gap-0.5">
                       {Object.entries(colors).map(([shade, color]) => {
                         return (
                           <ColorSwatch
@@ -127,7 +138,7 @@ export function ColorSelectorPopover({
                             isSelected={isColorSelected(color)}
                             onClick={() => handleColorSelect(color)}
                             className="rounded-none"
-                            size="sm"
+                            size="md"
                           />
                         );
                       })}
@@ -160,8 +171,8 @@ function ColorSwatch({
 }: ColorSwatchProps) {
   const sizeClasses = {
     sm: "size-5",
-    md: "size-8",
-    lg: "size-10",
+    md: "size-6",
+    lg: "size-8",
   };
   return (
     <TooltipWrapper asChild label={`${name} - ${color}`}>
